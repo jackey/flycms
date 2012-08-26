0ba3f596d61f32d4b7b35edb692901689921c94f
Jackey chen <jziwenchen@gmail.com>
2012-08-26 14:06:04 +0800

    move requirejs config into config.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,33 +1,5 @@
-require.config({
-	baseUrl: '/',
-	paths: {
-		backbone: 'lib/backbone',
-		jquery: 'lib/jquery',
-		underscore: 'lib/underscore',
-		templates: 'theme/default',
-		app: 'app',
-		fly: 'plugins/core/fly',
-		layoutmanager: 'lib/backbone.layoutmanager',
-		queryparams: 'lib/backbone.queryparams',
-		jquerycookie: 'lib/jquery.cookie'
-	},
-	shim: {
-		layoutmanager: ['backbone'],
-		queryparams: ['backbone'],
-		jquerycookie: ['jquery'],
-		backbone: {
-			deps: ['underscore', 'jquery', 'jquerycookie'],
-			exports: 'Backbone'
-		},
-		underscore: {
-			exports: '_'
-		},
-		jquery: {
-			exports: '$'
-		}
-	}
-});
-
-require(['app'], function (App) {
-	App.initialize();
+define(['./config'], function (config) {
+	require(['app'], function (App) {
+		App.initialize();
+	});
 });
---
+++ config.js
@@ -0,0 +1,29 @@
+require.config({
+	baseUrl: '/',
+	paths: {
+		backbone: 'lib/backbone',
+		jquery: 'lib/jquery',
+		underscore: 'lib/underscore',
+		templates: 'theme/default',
+		app: 'app',
+		fly: 'plugins/core/fly',
+		layoutmanager: 'lib/backbone.layoutmanager',
+		queryparams: 'lib/backbone.queryparams',
+		jquerycookie: 'lib/jquery.cookie'
+	},
+	shim: {
+		layoutmanager: ['backbone'],
+		queryparams: ['backbone'],
+		jquerycookie: ['jquery'],
+		backbone: {
+			deps: ['underscore', 'jquery', 'jquerycookie'],
+			exports: 'Backbone'
+		},
+		underscore: {
+			exports: '_'
+		},
+		jquery: {
+			exports: '$'
+		}
+	}
+});
